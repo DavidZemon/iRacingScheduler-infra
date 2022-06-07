17f5ce7b5148a6dd928abbc755c42c86445cfa13
David Zemon <david@zemon.name>
2022-06-06 22:30:01 -0500

    Add auto-formatter

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region = "us-east-2"
+  region  = "us-east-2"
 }
 
 resource "aws_dynamodb_table" "season_schedule" {
-  name = "season_schedule"
+  name         = "season_schedule"
   billing_mode = "PAY_PER_REQUEST"
 }
